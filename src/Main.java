4984883b77baceeb4108f58725292f2f44a38267
Cristian Carreño <kiel@acid.cl>
2013-10-02 23:00:59 -0300

    Add Punto de partida, .javas varios

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author Richard Ibarra Ram’rez (richard.ibarra@gmail.com)
+ * 
+ *  CC5303 - Primavera 2013
+ *  C‡tedra. Javier Bustos.
+ *  DCC. Universidad de Chile
+ */
+
+package cl.dcc.cc5303;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pong pong = new Pong();
+	}
+}
