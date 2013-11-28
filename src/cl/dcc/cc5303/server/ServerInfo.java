ed0bfe6b2281407d55d2e6982debfb514278595f
smaass <sergio.maass@gmail.com>
2013-11-28 16:05:23 -0300

    ServerInfo es serializable

--- src/cl/dcc/cc5303/server/ServerInfo.java
+++ src/cl/dcc/cc5303/server/ServerInfo.java
@@ -1,6 +1,9 @@
 package cl.dcc.cc5303.server;
 
-public class ServerInfo {
+import java.io.Serializable;
+
+public class ServerInfo implements Serializable {
+	private static final long serialVersionUID = 1484395313255285563L;
 	public int matches;
 	public int inmigrations;
 	public int serverID;
